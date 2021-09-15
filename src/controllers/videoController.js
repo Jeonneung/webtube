da019e348a36ddc59518c86d681bcf1249bcdaea
Jeonneung <worrymoon@naver.com>
2021-09-15 19:15:22 +0900

    controllers

---
+++ src/controllers/videoController.js
@@ -0,0 +1,5 @@
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("Watch");
+export const edit = (req, res) => res.send("Edit");
+export const deleteVideo = (req, res) => res.send("Delete");
+export const upload = (req, res) => res.send("Upload");
