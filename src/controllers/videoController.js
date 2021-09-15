e769263c0231454ce1f3177b5d3d6870c630c641
Jeonneung <worrymoon@naver.com>
2021-09-15 20:40:34 +0900

    views updated: render

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,7 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) =>
+  res.render("Watch", { pageTitle: "Watch Videos" });
+export const edit = (req, res) =>
+  res.render("Edit", { pageTitle: "Edit Videos" });
 export const deleteVideo = (req, res) => res.send("Delete");
 export const upload = (req, res) => res.send("Upload");
