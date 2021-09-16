3fd84aa690e467af65ed1b99423c56ce4f8b1380
Jeonneung <worrymoon@naver.com>
2021-09-16 22:11:03 +0900

    mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,34 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) =>
-  res.render("Watch", { pageTitle: "Watch Videos" });
-export const edit = (req, res) =>
-  res.render("Edit", { pageTitle: "Edit Videos" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      ratinf: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 22,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      ratinf: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 22,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      ratinf: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 22,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home" });
+};
+export const see = (req, res) => res.render("Watch");
+export const edit = (req, res) => res.render("Edit");
 export const deleteVideo = (req, res) => res.send("Delete");
 export const upload = (req, res) => res.send("Upload");
+export const search = (req, res) => res.send("Search");
