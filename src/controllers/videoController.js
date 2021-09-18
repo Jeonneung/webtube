9ceb9a6c50139e1e63d6d2d79b7c7698174a779a
Jeonneung <worrymoon@naver.com>
2021-09-18 23:27:19 +0900

    usedFakeDB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,46 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 22,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 22,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 22,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      ratinf: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 22,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      ratinf: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 22,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      ratinf: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 22,
-      id: 1,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home" });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("Watch");
-export const edit = (req, res) => res.render("Edit");
-export const deleteVideo = (req, res) => res.send("Delete");
-export const upload = (req, res) => res.send("Upload");
-export const search = (req, res) => res.send("Search");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("Watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("Edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
