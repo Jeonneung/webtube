0c6d39280344b500d458c96bf6b42dbd21683b52
Jeonneung <worrymoon@naver.com>
2021-09-24 21:56:15 +0900

    schema detail

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/video";
+import videoRouter from "../routers/videoRouter";
 
 /*
 callback way
@@ -6,18 +7,18 @@ callback way
   if(error){
     return res. render("server-error")
   }
-  return return res.render("home", { pageTitle: "Home", videos });
+   return res.render("home", { pageTitle: "Home", videos });
 });
 */
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("Watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  return res.render("Watch", { pageTitle: video.title, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -35,15 +36,17 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
