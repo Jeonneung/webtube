74c4dd53f61f8e4ffbc8b8bc6629f39c39c7ad85
Jeonneung <worrymoon@naver.com>
2021-09-27 22:14:02 +0900

    search

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/webtube", {
-  useNewUrlParser: true,
+  /*useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
+  useFindAndModify: false, 이 코드들은 mongoose가 6.0 버전 이상이기 때문에 항상 이 코드를 기억하고 실행하여 더이상 지원하지 않는다.*/
 });
 
 const db = mongoose.connection;
